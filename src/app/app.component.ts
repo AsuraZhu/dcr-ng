512a49acfd1321ff4636615232e7d1803129c4fc
zmm <1614180462@qq.com>
2017-12-28 18:24:42 +0800

    删除无用代码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,31 +10,7 @@ import { OnInit } from '@angular/core';
   ,
 })
 export class AppComponent implements OnInit {
-  products =  [
-    new Product(1, '第一个商品', 1.99, 3.5, '这是第一个商品,是我在学习慕课网Angular入门实战时创建的', ['电子产品', '硬件设备'])
-  ];
   title = 'app';
   ngOnInit(): void {
-    console.log('初始化');
-
-    const products = [
-      new Product(1, '第一个商品', 1.99, 3.5, '这是第一个商品,是我在学习慕课网Angular入门实战时创建的', ['电子产品', '硬件设备'])
-    ];
-
-    console.log(products);
-  }
-}
-
-
-export class Product {
-  constructor(
-    public id: number,
-    public title: string,
-    public price: number,
-    public rating: number,
-    public desc: string,
-    public categories: Array<string>
-  ) {
-
   }
 }
