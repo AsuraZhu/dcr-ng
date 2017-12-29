18476091ab22bc727a37464fb92a1556510e8c0a
zmm <1614180462@qq.com>
2017-12-29 12:02:25 +0800

    添加注册接口服务

---
+++ src/app/shared/services/register.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * 获取极验验证码
+ * @constructor
+ */
+
+ @Injectable()
+ export class RegisterService {
+    private url = 'http://batpool.dev.ailadui.net/v1/api/user/public/checkgt?time=' + (new Date()).getTime();
+    private handleError(error: any): Promise<any> {
+      return Promise.reject(error.message || error);
+    }
+    constructor(private http: HttpClient) {}
+    /**
+     * 获取手机验证码
+     * @返回参数 是{}
+     * @returns {} :any
+     */
+    getCode(): Promise<any> {
+      return this.http.get(this.url)
+                 .toPromise()
+                 .then(response => {
+                  return response;
+                 })
+                 .catch(this.handleError);
+    }
+    //
+    register(): Promise<any> {
+      return this.http.post(this.url, {})
+                 .toPromise()
+                 .then(response => {
+                    return response;
+                 })
+                 .catch(this.handleError);
+    }
+ }
